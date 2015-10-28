60cde881a6e32935dbee719ddcf277d38fd1a99c
Martin Malchow <martin.malchow@hpi.de>
2015-10-28 12:54:39 +0100

    Chenge description

--- upload.php
+++ upload.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * upload.php
+ * upload.php file to handle chunked upload
  *
  * Copyright 2013, Moxiecode Systems AB (Original Author)
  * Released under GPL License.
